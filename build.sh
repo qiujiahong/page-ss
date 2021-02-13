b40de09242e7661cbb5a6debb2cdfafdacf56a47
Nick <qiujiahongde@163.com>
2021-02-13 11:26:27 +0800

    添加配置文件到发布包内

--- build.sh
+++ build.sh
@@ -4,33 +4,33 @@ repo=page-ss
 rm -rf dist/
 # 打包mac版本程序
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build ./
-mkdir -p dist/${version}/${repo}_mac
-mv ${repo} dist/${version}/${repo}_mac
-cp ./script/run.sh dist/${version}/${repo}_mac
-#cp -rf conf dist/${version}/${repo}_mac
-cp docs/help.md dist/${version}/${repo}_mac
+mkdir -p dist/"${version}"/${repo}_mac
+mv ${repo} dist/"${version}"/${repo}_mac
+cp ./script/run.sh dist/"${version}"/${repo}_mac
+cp -rf conf dist/"${version}"/${repo}_mac
+cp docs/help.md dist/"${version}"/${repo}_mac
 
 
 # 打包Linux 版本程序
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build ./
-mkdir -p dist/${version}/${repo}_linux
-mv ${repo} dist/${version}/${repo}_linux
-cp ./script/run.sh dist/${version}/${repo}_linux
-#cp -rf conf dist/${version}/${repo}_linux
-cp docs/help.md dist/${version}/${repo}_linux
+mkdir -p dist/"${version}"/${repo}_linux
+mv ${repo} dist/"${version}"/${repo}_linux
+cp ./script/run.sh dist/"${version}"/${repo}_linux
+cp -rf conf dist/"${version}"/${repo}_linux
+cp docs/help.md dist/"${version}"/${repo}_linux
 
 
 # 打包windows版本程序
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build ./
-mkdir -p dist/${version}/${repo}_win
-mv ${repo}.exe dist/${version}/${repo}_win
-cp ./script/run.bat dist/${version}/${repo}_win
-#cp -rf conf dist/${version}/${repo}_win
-cp docs/help.md dist/${version}/${repo}_win
+mkdir -p dist/"${version}"/${repo}_win
+mv ${repo}.exe dist/"${version}"/${repo}_win
+cp ./script/run.bat dist/"${version}"/${repo}_win
+cp -rf conf dist/"${version}"/${repo}_win
+cp docs/help.md dist/"${version}"/${repo}_win
 
 # 拷贝
 
-cp -rf dist/${version}  dist/latest
+cp -rf dist/"${version}"  dist/latest
 
 cd dist
-tar czvf ${repo}-${version}.tar.gz ${version}
+tar czvf ${repo}-"${version}".tar.gz "${version}"
