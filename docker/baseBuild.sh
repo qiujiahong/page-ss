9e013c057647a79d157443bfea094ef93617f267
Nick <qiujiahongde@163.com>
2021-02-14 16:46:21 +0800

    删除docker文件夹内的应用程序包

--- docker/baseBuild.sh
+++ docker/baseBuild.sh
@@ -2,4 +2,4 @@
 
 docker build  -f docker/Base.Dockerfile -t qiujiahong/chromedp:v1.0 .
 
-docker push qiujiahong/ch romedp:v1.0
+docker push qiujiahong/chromedp:v1.0
--- docker/build.sh
+++ docker/build.sh
@@ -9,4 +9,5 @@ cd docker
 docker build -t qiujiahong/page-ss:v1.0 .
 
 
+
 # docker run -p 8080:8080  qiujiahong/page-ss:v1.0
