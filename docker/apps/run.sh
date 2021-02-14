89cde17666ca777885fb60a891859caa7fd4f29c
Nick <qiujiahongde@163.com>
2021-02-14 16:40:52 +0800

    删除可执行文件

--- docker/apps/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# 后台启动
-chmod +x page-ss
-./page-ss -d=true
--- docker/build.sh
+++ docker/build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 
-#sh build.sh
-#rm -rf docker/apps
-#cp -rf ./dist/latest/page-ss_linux docker/apps
+sh build.sh
+rm -rf docker/apps
+cp -rf ./dist/latest/page-ss_linux docker/apps
 cd docker
 
 docker build -t qiujiahong/page-ss:v1.0 .
 
+
+# docker run -p 8080:8080  qiujiahong/page-ss:v1.0
